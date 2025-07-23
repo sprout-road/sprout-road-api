a9384f70186c0310378eaf7f647ef2c11a53bd4e
jihwankim128 <jihwan1535@nate.com>
2025-07-23 20:13:06 +0900

    feat: postgre db setting & entity 추가

---
+++ src/main/java/com/strout/api/config/database/JpaConfig.java
@@ -0,0 +1,9 @@
+package com.strout.api.config.database;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaConfig {
+}
