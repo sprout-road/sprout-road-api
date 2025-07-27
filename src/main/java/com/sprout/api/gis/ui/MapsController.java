47e41cd04b254c431f3d0148feff9adeb8a69ee2
jihwankim128 <jihwan1535@nate.com>
2025-07-27 13:17:08 +0900

    test: 서버 다운 이슈 - 부하 테스트 진행
    * 배포 시 서버 메모리 문제가 발생
    * json으로 미리 cdn에서 관리하고 서버는 cdn을 반환하도록 개선
      * 컨트롤러는 cdn 주소만 필요해서 합침
    * 과정 중 monitoring으로 메모리 안정 테스트 완료
    * k6 스크립트로 부하테스트 완료

---
+++ src/main/java/com/sprout/api/gis/ui/MapsController.java
@@ -0,0 +1,40 @@
+package com.sprout.api.gis.ui;
+
+import jakarta.validation.constraints.Pattern;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/gis")
+@Slf4j
+public class MapsController {
+
+    @Value("${app.storage.cdn.uri}")
+    private String cdnUri;
+
+    @GetMapping("/sido")
+    public ResponseEntity<byte[]> getAllSido() {
+        log.info(" =========> sido ");
+        String location = String.format("%s/maps/sido.json", cdnUri);
+        return ResponseEntity.status(HttpStatus.MOVED_PERMANENTLY)
+            .header(HttpHeaders.LOCATION, location)
+            .build();
+    }
+
+    @GetMapping("/sigungu/{sidoCode}")
+    public ResponseEntity<byte[]> getSigunguBySidoCode(@PathVariable @Pattern(regexp = "\\d{2}") String sidoCode) {
+        String location = String.format("%s/maps/sigungu_%s.json", cdnUri, sidoCode);
+        return ResponseEntity.status(HttpStatus.MOVED_PERMANENTLY)
+            .header(HttpHeaders.LOCATION, location)
+            .build();
+    }
+}
