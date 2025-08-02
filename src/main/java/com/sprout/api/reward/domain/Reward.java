ce07eebc4ad264a3c6ca52f8394b7b4ef5421954
jihwankim128 <jihwan1535@nate.com>
2025-08-03 00:05:38 +0900

    feat: reward domain layer

---
+++ src/main/java/com/sprout/api/reward/domain/Reward.java
@@ -0,0 +1,42 @@
+package com.sprout.api.reward.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(
+    name = "rewards",
+    uniqueConstraints = @UniqueConstraint(name = "uk_region_code_name", columnNames = {"regionCode, regionName"}))
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
+public class Reward {
+
+    @Id
+    private Long id;
+    @Column(nullable = false)
+    private String regionCode;
+    @Column(nullable = false)
+    private String regionName;
+    @Column(nullable = false)
+    private String imageUrl;
+
+    public static Reward create(String regionCode, String regionName, String imageUrl) {
+        return Reward.builder()
+            .regionCode(regionCode)
+            .regionName(regionName)
+            .imageUrl(imageUrl)
+            .build();
+    }
+
+    public void updateImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+}
